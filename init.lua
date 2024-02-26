20dde19c98c03a59f9ad8f9837708bc8a1d82256
wojciech29 <vojtech.remes@gmail.com>
2024-02-26 13:03:59 +0100

    lsp shortcuts

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ require('core.plugins-setup')
 
 local augroup = vim.api.nvim_create_augroup
 local DefaultGroup = augroup('DefaultGroup', {})
-
 local autocmd = vim.api.nvim_create_autocmd
 local TelescopeColor = {
     TelescopeMatching = { fg = '#f6c177' },
@@ -13,12 +12,6 @@ local TelescopeColor = {
 for hl, col in pairs(TelescopeColor) do
     vim.api.nvim_set_hl(0, hl, col)
 end
-
-vim.filetype.add({
-    extension = {
-        templ = 'templ',
-    }
-})
 
 -- remove trailing whitespace on save
 autocmd({"BufWritePre"}, {
@@ -33,9 +26,7 @@ autocmd('LspAttach', {
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end

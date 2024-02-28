fdba77248670c6444576f79e9b2755117024aa91
wojciech29 <vojtech.remes@gmail.com>
2024-02-28 13:59:37 +0100

    telescope

--- init.lua
+++ init.lua
@@ -2,13 +2,21 @@ require('core.options')
 require('core.keymaps')
 require('core.plugins-setup')
 
-vim.cmd.colorscheme 'tokyonight'
+-- vim.cmd.colorscheme 'kanagawa'
+vim.cmd.colorscheme 'duskfox'
+local TelescopeColor = {
+    TelescopeMatching = { fg = '#f6c177' },
+}
+
+for hl, col in pairs(TelescopeColor) do
+    vim.api.nvim_set_hl(0, hl, col)
+end
 
 local DefaultGroup = vim.api.nvim_create_augroup('DefaultGroup', {})
 local autocmd = vim.api.nvim_create_autocmd
 
 -- remove trailing whitespace on save
-autocmd({"BufWritePre"}, {
+autocmd({ "BufWritePre" }, {
     group = DefaultGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -3,14 +3,46 @@ return {
         'EdenEast/nightfox.nvim',
     },
     {
+        'rebelot/kanagawa.nvim',
+        config = function()
+            require('kanagawa').setup({
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none"
+                            }
+                        }
+                    }
+                }
+            })
+        end
+    },
+    {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-    },
-    {
-        "mcchrish/zenbones.nvim",
-        dependencies = {
-            "rktjmp/lush.nvim"
-        },
+        config = function()
+            require("tokyonight").setup({
+                on_highlights = function(hl, c)
+                    hl.TelescopeBorder = {
+                        bg = c.bg_dark,
+                        fg = c.fg_gutter,
+                    }
+                    hl.TelescopePromptTitle = {
+                        bg = c.bg_dark,
+                        fg = c.fg_gutter,
+                    }
+                    hl.TelescopePreviewTitle = {
+                        bg = c.bg_dark,
+                        fg = c.fg_gutter,
+                    }
+                    hl.TelescopeResultsTitle = {
+                        bg = c.bg_dark,
+                        fg = c.fg_gutter,
+                    }
+                end,
+            })
+        end
     },
 }

e4db00068c641e2efecd9aee0dbcea9941a70062
wojciech29 <vojtech.remes@gmail.com>
2024-03-05 09:30:16 +0100

    kickstart

--- init.lua
+++ init.lua
@@ -1,76 +1,45 @@
--- Set <space> as the leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Disable netrw (default file explorer)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- [[ Setting options ]] `:help option-list`
--- Make line numbers default
+vim.opt.termguicolors = true
 vim.opt.number = true
-
--- Add relative line numbers, for help with jumping.
 vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
-
--- Don't show the mode, since it's already in status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
-vim.opt.clipboard = "unnamedplus"
-
--- Enable break indent
+vim.opt.mouse = "a" -- "a" is all modes, "n" is normal mode only
+vim.opt.showmode = false -- Don't show the mode, since it's already in status line
+vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim.
 vim.opt.breakindent = true
-
--- Save undo history
+vim.opt.swapfile = false
+vim.opt.backup = false
 vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
 vim.opt.signcolumn = "yes"
-
--- Decrease update time
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Tab config
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.smartcase = true
-
--- Sets how neovim will display certain whitespace in the editor.
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
-
--- Show which line your cursor is on
 vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.scrolloff = 8
+vim.opt.incsearch = true
 vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- [[ Basic Keymaps ]]
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Search and replace of current word
 vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "[R]eplace [W]ord" })
+
+-- Move lines up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Navigation keymaps
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -580,8 +549,6 @@ require("lazy").setup({
 
 				-- For an understanding of why these mappings were
 				-- chosen, you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 
 				mapping = cmp.mapping.preset.insert({
 					-- Select the [n]ext item
@@ -589,15 +556,15 @@ require("lazy").setup({
 					-- Select the [p]revious item
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 
-					-- Accept the completion.
+					-- Accept ([y]es) the completion.
 					--  This will auto-import if your LSP supports it.
 					--  This will expand snippets if the LSP sent a snippet.
-					["<C-Space>"] = cmp.mapping.confirm({ select = true }),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
 					--  completions whenever it has completion options available.
-					["<C-y"] = cmp.mapping.complete({}),
+					["<C-Space>"] = cmp.mapping.complete({}),
 
 					-- Think of <c-l> as moving to the right of your snippet expansion.
 					--  So if you have a snippet that's like:
@@ -741,7 +708,6 @@ require("lazy").setup({
 					"vimdoc",
 					"lua",
 					"markdown",
-					"html",
 					"htmldjango",
 					"css",
 					"javascript",
@@ -780,7 +746,7 @@ require("lazy").setup({
 		config = function()
 			require("treesitter-context").setup({
 				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
 				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 				line_numbers = true,
 				multiline_threshold = 20, -- Maximum number of lines to show for a single context
--- lua/core/keymaps.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]])
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
-vim.keymap.set("n", "<leader>=", vim.lsp.buf.format)
